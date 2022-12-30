1fda0d5f235d57387c3dbd3a4fbcf2fefe0ab41d
Martin Rodriguez <martinrodriguezgervasio@gmail.com>
2022-12-30 17:46:31 -0300

    feat(rankpage designs): first draft of rank page designs with hardcoded values

--- src/pages/rank/[rankName].tsx
+++ src/pages/rank/[rankName].tsx
@@ -1,19 +1,25 @@
 import { RankItem } from "@prisma/client";
 import { type NextPage } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { HiDotsVertical } from "react-icons/hi";
 
 import { trpc } from "../../utils/trpc";
 import { unKebab } from "../../utils/unkebab";
 
+import GoldMedal from "../../../public/medal-gold.webp";
+import SilverMedal from "../../../public/medal-silver.webp";
+import BronzeMedal from "../../../public/medal-bronze.webp";
+import { GoComment } from "react-icons/go";
+
 const borderColor = (rank: number) => {
   switch (rank) {
     case 1:
-      return "bg-[#C9B037]";
+      return "bg-[#181303]";
     case 2:
-      return "bg-[#B4B4B4]";
+      return "bg-[#121212]";
     case 3:
-      return "bg-[#AD8A56]";
+      return "bg-[#120B04]";
     default:
       return "bg-black";
   }
@@ -22,11 +28,11 @@ const borderColor = (rank: number) => {
 const rankPrefix = (rank: number) => {
   switch (rank) {
     case 1:
-      return <span className="pb-2 text-lg tracking-wider text-gray">🥇</span>;
+      return <Image className="w-10" src={GoldMedal} alt="rank 1" />;
     case 2:
-      return <span className="pb-2 text-lg tracking-wider text-gray">🥈</span>;
+      return <Image className="w-10" src={SilverMedal} alt="rank 2" />;
     case 3:
-      return <span className="pb-2 text-lg tracking-wider text-gray">🥉</span>;
+      return <Image className="w-10" src={BronzeMedal} alt="rank 3" />;
     default:
       return <span className="text-xs tracking-wider text-gray">#{rank}</span>;
   }
@@ -35,7 +41,7 @@ const rankPrefix = (rank: number) => {
 const RankItem = ({ item, rank }: { item: RankItem; rank: number }) => {
   return (
     <div
-      className={`flex w-full flex-col gap-3 border-b border-solid border-gray ${borderColor(
+      className={`flex w-full flex-col border-b border-solid border-gray ${borderColor(
         rank
       )} py-2 px-3 shadow-lg first:border-t sm:border-x`}
     >
@@ -44,7 +50,14 @@ const RankItem = ({ item, rank }: { item: RankItem; rank: number }) => {
           {rankPrefix(rank)}
           <h2 className="text-md tracking-wide">{item.name}</h2>
         </div>
-        <HiDotsVertical className="text-gray" />
+        <p className="text-xs tracking-wide">71M (30%)</p>
+      </div>
+      <div className="flex items-center justify-between text-xs text-gray">
+        <q>The true G.O.A.T. is here</q>
+        <div className="flex items-center justify-center gap-1">
+          <GoComment className="pt-0.5" />
+          <p className="tracking-wide">100K</p>
+        </div>
       </div>
     </div>
   );
@@ -63,7 +76,7 @@ const Rank: NextPage = () => {
   return (
     <>
       <main className="w-full flex-1 sm:w-3/4 md:w-3/5 lg:w-2/5">
-        <div className="px-4 pt-10">
+        <div className="px-4 pt-10 pb-2">
           <h1 className="text-lg tracking-wide">
             {unKebab(rankQuery.data?.name)}
           </h1>
